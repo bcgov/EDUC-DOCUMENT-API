bde5f1403ae8966a71ee3a28d9508c2bdd0784fc
Mingwei <mingwei.y@gmail.com>
2019-12-20 15:57:50 -0800

    Add OAuth2 to secure the service

--- api/src/main/java/ca/bc/gov/educ/api/document/controller/DocumentController.java
+++ api/src/main/java/ca/bc/gov/educ/api/document/controller/DocumentController.java
@@ -3,12 +3,23 @@ package ca.bc.gov.educ.api.document.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.UUID;
 
 @RestController
-//@RequestMapping
+@RequestMapping("/")
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableResourceServer
 public class DocumentController {
-    @GetMapping
-    public String index() {
-        return "Document API";
+    
+    @GetMapping("/{documentID}")
+    @PreAuthorize("#oauth2.hasScope('READ_DOCUMENT')")
+    public String readDocument(@PathVariable UUID documentID) {
+        return "Document: " + documentID.toString();
     }
 }
